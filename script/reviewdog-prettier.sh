eed4a163be7413ef5c7836603b98d1546d344179
David Rodríguez <2887858+deivid-rodriguez@users.noreply.github.com>
2025-10-22 12:15:23 +0200

    Check like this

--- script/reviewdog-prettier.sh
+++ script/reviewdog-prettier.sh
@@ -4,7 +4,7 @@
 # Runs prettier and pipes its output to reviewdog so it annotates a pull request with the issues found
 #
 
-yarn run prettier --check . 2>&1 | sed --regexp-extended 's/(\[warn\].*)$/\1 File is not properly formatted./' \
+"$(npm root)/.bin/prettier" --check . 2>&1 | sed --regexp-extended 's/(\[warn\].*)$/\1 File is not properly formatted./' \
   | reviewdog \
       -efm="%-G[warn] Code style issues found in the above file(s). Forgot to run Prettier%. File is not properly formatted." \
       -efm="[%tarn] %f %m" \
